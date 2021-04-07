fe9a617035028e7f40f92fcd2897308193f12ae3
Unquieterpig <poopbutt3123131@gmail.com>
2021-04-07 16:58:24 -0700

    Exception to colorama import organized imports

--- threaded-humidity.py
+++ threaded-humidity.py
@@ -3,9 +3,14 @@ import json
 import csv
 import os
 import configparser
-import colorama
 import re
 import threading
+
+from plotly import offline
+from datetime import datetime
+from time import sleep
+from sys import platform, version_info
+from hourly_average_NODAYS_class import HourlyPlot
 
 try:
     import plotly.express as px
@@ -17,13 +22,11 @@ try:
 except:
     print("plotly not found to install: pip3 install pandas")
     exit(-1)
-
-from plotly import offline
-from datetime import datetime
-from time import sleep
-from sys import platform, version_info
-
-from hourly_average_NODAYS_class import HourlyPlot
+try:
+    import colorama
+except:
+    print("colorama not found to install: pip3 install colorama")
+    exit(-1)
 
 #Initial check to see if they are running 3.6 or greater
 version_major = version_info.major
@@ -31,7 +34,7 @@ version_minor = version_info.minor
 
 if version_major < 3 or (version_major == 3 and version_minor < 6):
     print('This program requires Python 3.6 and above, please consider upgrading')
-    exit(1)
+    exit(-1)
 
 #Initializes colorama to use text colors!
 colorama.init()
